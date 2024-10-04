0ef7372f8256d47a26a4d16cc2411dadf93b568b
ndchien <ndchien25402@gmail.com>
2024-10-04 17:30:16 +0700

    config octance

--- app/Http/Resources/NotificationResource.php
+++ app/Http/Resources/NotificationResource.php
@@ -5,6 +5,19 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     schema="NotificationResource",
+ *     type="object",
+ *     title="NotificationResource",
+ *     required={"id", "data", "type", "created_at"},
+ *     @OA\Property(property="id", type="integer", example=1),
+ *     @OA\Property(property="data", type="object", example={"message": "You have a new notification"}),
+ *     @OA\Property(property="type", type="string", example="info"),
+ *     @OA\Property(property="created_at", type="string", format="date-time", example="2023-10-04T10:00:00Z"),
+ *     @OA\Property(property="read_at", type="string", format="date-time", example="2023-10-04T12:00:00Z"),
+ * )
+ */
 class NotificationResource extends JsonResource
 {
     /**
